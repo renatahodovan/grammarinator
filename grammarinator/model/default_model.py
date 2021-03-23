ee29474970b469f147d07b30879590970860d4f2
Hodovan Renata <reni@inf.u-szeged.hu>
2021-03-23 23:49:19 +0100

    Remove superfluous normalization from the choice method

--- grammarinator/model/default_model.py
+++ grammarinator/model/default_model.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2020-2021 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -12,8 +12,6 @@ class DefaultModel(object):
 
     def choice(self, node, idx, choices):
         # assert sum(choices) > 0, 'Sum of choices is zero.'
-        max_item = max(choices)
-        choices = [w / max_item for w in choices]
         r = random.uniform(0, sum(choices))
         upto = 0
         for i, w in enumerate(choices):
