28c3cffc9225d99c2ab14f81debb12477819e764
Akos Kiss <akiss@inf.u-szeged.hu>
2021-08-24 15:23:59 +0200

    Use importlib.metadata to retrieve package version
    (Use importlib_metadata backport for <py38.) This allows to get rid
    of pkg_resources/setuptools install dependency.

--- grammarinator/pkgdata.py
+++ grammarinator/pkgdata.py
@@ -5,7 +5,10 @@
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
-import pkg_resources
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
 
 
-__version__ = pkg_resources.get_distribution(__package__).version
+__version__ = metadata.version(__package__)
