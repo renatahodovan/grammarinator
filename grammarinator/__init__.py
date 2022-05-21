4cde1d95c9ffd27f09a2d228b6da6f7c531c087a
Renata Hodovan <reni@inf.u-szeged.hu>
2022-05-21 16:55:51 +0200

    Publish Generator class from generate to support using it from API

--- grammarinator/__init__.py
+++ grammarinator/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2020 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2022 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -6,6 +6,7 @@
 # according to those terms.
 
 from . import runtime
+from .generate import Generator
 from .parse import ParserFactory
 from .pkgdata import __version__
 from .process import FuzzerFactory
