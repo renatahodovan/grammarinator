dd42a94be6b363979f9ea0629e8cc6918da97744
Akos Kiss <akiss@inf.u-szeged.hu>
2024-12-12 14:47:42 +0100

    Don't import importlib_metadata since Py3.8 already has it as importlib.metadata
    It has already been dropped from setup.cfg.

--- grammarinator/pkgdata.py
+++ grammarinator/pkgdata.py
@@ -1,14 +1,11 @@
-# Copyright (c) 2017-2021 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2024 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
-try:
-    from importlib import metadata
-except ImportError:
-    import importlib_metadata as metadata
+from importlib import metadata
 
 
 __version__ = metadata.version(__package__)
