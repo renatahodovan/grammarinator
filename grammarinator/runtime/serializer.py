d6caa88f99de2f5460ce7bb623a54e1369751e54
Renáta Hodován <reni@inf.u-szeged.hu>
2024-12-20 19:11:22 +0100

    Simplify simple_space_serializer using `tokens()` method (#263)
    The new `tokens()` method of rule nodes makes it easier to access
    their leaf node sequences. This update leverages the `tokens()`
    method to simplify the implementation of `simple_space_serializer`.

--- grammarinator/runtime/serializer.py
+++ grammarinator/runtime/serializer.py
@@ -1,11 +1,9 @@
-# Copyright (c) 2017-2023 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2024 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
-
-from .rule import ParentRule
 
 
 def simple_space_serializer(root):
@@ -17,14 +15,4 @@ def simple_space_serializer(root):
     :return: The serialized tree as string.
     :rtype: str
     """
-
-    def _tokens():
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if isinstance(node, ParentRule):
-                stack.extend(reversed(node.children))
-            else:
-                yield node.src
-
-    return ' '.join(token for token in _tokens())
+    return ' '.join(root.tokens())
