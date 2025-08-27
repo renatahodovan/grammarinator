d797bf936ddbb5a94ed2b35e6f2f2e08388bb643
Renáta Hodován <reni@inf.u-szeged.hu>
2025-08-27 23:37:36 +0200

    Add examples for HTML serializer and config in C++ (#301)

---
+++ examples/fuzzer/HTMLConfig.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 Renata Hodovan, Akos Kiss.
+//
+// Licensed under the BSD 3-Clause License
+// <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
+// This file may not be copied, modified, or distributed except
+// according to those terms.
+
+#include "HTMLGenerator.hpp"
+#include "HTMLSpaceSerializer.hpp"
